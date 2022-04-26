cb8e22e08407ef36f72bcf9d06944dce289eeac7
Dawid Miszczuk <miszczukd@gmail.com>
2022-04-26 07:44:42 +0200

    html edit

--- html/script.js
+++ html/script.js
@@ -4,7 +4,7 @@ function ini_spells_short(data){
 
     html = '';
     prev_class = '';
-    prev_lvl = 0;
+    prev_lvl = '';
 
     $.each(data, function(key, value){
 
@@ -26,16 +26,35 @@ function ini_spells_short(data){
                 html += '</div>';
                 html += '<div class = "class_content spells_short '+value.CLASS+'">';
         }
-                    html += '<div class = "spell_container spells_short '+value.CLASS+'">';
-                        html += '<div class = "spell_label spells_short '+value.CLASS+'">';
-                            html += value.SPELL_NAME;  
-                        html += '</div>';  
-                        html += '<div class = "spell_content spells_short '+value.CLASS+'">';
-                            html += value.SPELL_SHORT_DESCRIPTION;  
-                        html += '</div>';  
-                    html += '</div>';
+            if(prev_lvl != SPELL_LEVEL){
 
-        
+                if( key != 0 ){
+                html += '</div>'; //level_container end
+                }
+
+                html += '<div class = "level_container spells_short '+value.CLASS+' '+value.SPELL_LEVEL+'">';
+                    html += '<div class = "level_label spells_short '+value.CLASS+' '+value.SPELL_LEVEL+'">';
+                        html += 'Poziom czaru:  <b>' + value.SPELL_LEVEL + '</b>';  
+                    html += '</div>';
+                    html += '<div class = "level_content spells_short '+value.CLASS+' '+value.SPELL_LEVEL+'">';
+            }
+                        html += '<div class = "spell_container spells_short '+value.CLASS+' '+value.SPELL_LEVEL+'">';
+                            html += '<div class = "spell_label spells_short '+value.CLASS+' '+value.SPELL_LEVEL+'">';
+                                html += value.SPELL_NAME;  
+                            html += '</div>';  
+                            html += '<div class = "spell_content spells_short '+value.CLASS+' '+value.SPELL_LEVEL+'">';
+                                html += value.SPELL_SHORT_DESCRIPTION; 
+                                html += '<div class = "spell_long_description spells_short '+value.CLASS+' '+value.SPELL_LEVEL+'">';
+                                    html += "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";  
+                                html += '</div>';  
+                            html += '</div>';  
+                        html += '</div>';
+
+            
+                if(prev_lvl == SPELL_LEVEL && key == data.length - 1 && key != 0){
+                html += '</div>'; //level_container end
+                }
+
             if(prev_class == CLASS && key == data.length - 1 && key != 0){
                 html += '</div>'; //class_content end
             html += '</div>'; //class_container end
@@ -43,9 +62,16 @@ function ini_spells_short(data){
            
             
             prev_class = CLASS;
+            prev_lvl = SPELL_LEVEL;
             
         });
 
     $(".main_container.spells_short .main_content").append(html);
 
 }
+
+
+$(document).on("click", ".spell_container",function(){
+    $(this).find(".spell_long_description").toggleClass("active");
+    $(this).toggleClass("active");
+});
